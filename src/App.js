4f3e65fd76619901569e39e715c4df5bf6c2ee65
madpotato1713 <madpotato1713@gmail.com>
2019-12-31 16:23:17 +0900

    version change

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React, { Component } from "react";
-import classNames from 'classnames';
-import styles from './App.module.css'
+import SassComponent from "./SassComponent";
 
-const cx = classNames.bind(styles);
-console.log(cx);
 class App extends Component {
   render() {
-    return (
-      <div className={cx(styles.box, styles.blue)}></div>
-    );
+    return <SassComponent />;
   }
 }
 
