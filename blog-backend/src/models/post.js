6ef481938924e3f370fa79a634d6da8084ccf9fc
madpotato1713 <madpotato1713@gmail.com>
2020-05-28 02:10:51 +0900

    예제 진행 중

--- blog-backend/src/models/post.js
+++ blog-backend/src/models/post.js
@@ -5,10 +5,10 @@ const { Schema } = mongoose;
 const Post = new Schema({
     title: String,
     body: String,
-    tags: [String],
+    tags: [String], // 문자열의 배열
     publishedDate: {
         type: Date,
-        default: new Date(),
+        default: new Date() // 현재 날짜를 기본값으로 지정
     }
 });
 
