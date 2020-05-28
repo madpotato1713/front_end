0dea9114be8288803c2208037af5532af50e0419
madpotato1713 <madpotato1713@gmail.com>
2020-05-29 00:44:53 +0900

    블로그 예제 완성

---
+++ blog-frontend/src/containers/common/Base.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import LoginModalContainer from 'containers/modal/LoginModalContainer';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as baseActions from 'store/modules/base';
+
+class Base extends Component {
+    initialize = async () => {
+        // 로그인 상태 확인
+        const { BaseActions } = this.props;
+        if(localStorage.logged === "true") {
+            BaseActions.tempLogin();
+        }
+        BaseActions.checkLogin();
+    }
+
+    componentDidMount() {
+        this.initialize();
+    }
+
+    render() {
+        return (
+            <div>
+                <LoginModalContainer/>
+            </div>
+        )
+    }
+}
+
+export default connect(
+    null,
+    (dispatch) => ({
+        BaseActions: bindActionCreators(baseActions, dispatch)
+    })
+)(Base);
