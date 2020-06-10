43b3078226b51ae4008e6198d14bfbab76224eb8
madpotato1713 <madpotato1713@gmail.com>
2020-06-11 00:10:44 +0900

    프로젝트 틀 잡기

---
+++ src/paint/container/PaintContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PaintContainer extends Component {
+    render() {
+        return (
+            <div>그림판</div>
+        );
+    }
+}
+
+export default PaintContainer;
